4cb8869bb8d3c68491907e9dacf1004ff29a266c
smais007 <smaisshawon@gmail.com>
2025-01-01 03:01:02 +0600

    Happy new year

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -1,11 +1,25 @@
 "use client";
 import { Badge } from "@/components/ui/badge";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { GetFormatterForCurrency } from "@/lib/helpers";
 import { Period, Timeframe } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import HistoryPeriodSelector from "./HistoryPeriodSelector";
+import { useQuery } from "@tanstack/react-query";
+
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { cn } from "@/lib/utils";
+import CountUp from "react-countup";
 
 const History = ({ userSettings }: { userSettings: UserSettings }) => {
   const [timeframe, setTimeframe] = useState<Timeframe>("month");
@@ -17,6 +31,17 @@ const History = ({ userSettings }: { userSettings: UserSettings }) => {
   const formatter = useMemo(() => {
     return GetFormatterForCurrency(userSettings.currency);
   }, [userSettings.currency]);
+
+  const historyDataQuery = useQuery({
+    queryKey: ["history", "overview", period, timeframe],
+    queryFn: () =>
+      fetch(
+        `/api/history-data?timeframe=${timeframe}&year=${period.year}&month=${period.month}`
+      ).then((res) => res.json()),
+  });
+
+  const availableData =
+    historyDataQuery.data && historyDataQuery.data.length > 0;
 
   return (
     <div className="container">
@@ -48,9 +73,160 @@ const History = ({ userSettings }: { userSettings: UserSettings }) => {
             </div>
           </CardTitle>
         </CardHeader>
+        <CardContent>
+          {availableData && (
+            <ResponsiveContainer width={"100%"} height={300}>
+              <BarChart
+                height={300}
+                data={historyDataQuery.data}
+                barCategoryGap={5}
+              >
+                <defs>
+                  <linearGradient id="incomeBar" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset={"0"} stopColor="#10b981" stopOpacity={"1"} />
+                    <stop offset={"1"} stopColor="#10b981" stopOpacity={"0"} />
+                  </linearGradient>
+                  <linearGradient id="expenseBar" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset={"0"} stopColor="#ef4444" stopOpacity={"1"} />
+                    <stop offset={"1"} stopColor="#ef4444" stopOpacity={"0"} />
+                  </linearGradient>
+                </defs>
+                <CartesianGrid
+                  strokeDasharray="5 5"
+                  strokeOpacity={"0.2"}
+                  vertical={false}
+                />
+                <XAxis
+                  stroke="#888888"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                  padding={{ left: 5, right: 5 }}
+                  dataKey={(data) => {
+                    const { year, month, day } = data;
+                    const date = new Date(year, month, day || 1);
+                    if (timeframe === "year") {
+                      return date.toLocaleString("default", { month: "long" });
+                    }
+                    return date.toLocaleString("default", { day: "2-digit" });
+                  }}
+                />
+                <YAxis
+                  stroke="#888888"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <Bar
+                  dataKey={"income"}
+                  label="Income"
+                  fill="url(#incomeBar)"
+                  radius={4}
+                  className="cursor-pointer"
+                />
+                <Bar
+                  dataKey={"expense"}
+                  label="Expenses"
+                  fill="url(#expenseBar)"
+                  radius={4}
+                  className="cursor-pointer"
+                />
+                <Tooltip
+                  cursor={{ opacity: 0.1 }}
+                  content={(props) => (
+                    <CustomTooltip {...props} formatter={formatter} />
+                  )}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          )}
+          {!availableData && (
+            <Card className="flex h-[300px] flex-col items-center justify-center bg-background">
+              No data for the selected period
+              <p className="text-sm text-muted-foreground">
+                Try slecting a different period or add new transactions
+              </p>
+            </Card>
+          )}
+        </CardContent>
       </Card>
     </div>
   );
 };
 
 export default History;
+
+function CustomTooltip({ active, payload, formatter }: any) {
+  if (!active || !payload || payload.length === 0) {
+    return null;
+  }
+
+  const data = payload[0].payload;
+  const { expense, income } = data;
+
+  return (
+    <div className="min-w-[300px] rounded border bg-background p-4">
+      <TooltipRow
+        formatter={formatter}
+        label="Expenses"
+        value={expense}
+        bgColor="bg-red-500"
+        textColor="text-red-500"
+      />
+      <TooltipRow
+        formatter={formatter}
+        label="Income"
+        value={income}
+        bgColor="bg-green-500"
+        textColor="text-green-500"
+      />
+      <TooltipRow
+        formatter={formatter}
+        label="Balance"
+        value={income - expense}
+        bgColor="bg-gray-500"
+        textColor="text-foreground"
+      />
+    </div>
+  );
+}
+
+function TooltipRow({
+  formatter,
+  label,
+  value,
+  bgColor,
+  textColor,
+}: {
+  label: string;
+  textColor: string;
+  bgColor: string;
+  value: number;
+  formatter: Intl.NumberFormat;
+}) {
+  const formatterFn = useCallback(
+    (value: number) => {
+      return formatter.format(value);
+    },
+    [formatter]
+  );
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className={cn("h-4 w-4 rounded-full", bgColor)} />
+      <div className="flex w-full justify-between">
+        <p className="text-sm text-muted-foreground">{label}</p>
+        <div className={cn("text-sm font-bold", textColor)}>
+          <CountUp
+            duration={0.5}
+            preserveValue
+            end={value}
+            decimals={0}
+            formattingFn={formatterFn}
+            className="text-sm font-bold"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
